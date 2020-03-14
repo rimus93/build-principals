db1f0f3ae15575ebb5d5396fb89a2d8297f58078
rimus93 <45934179+rimus93@users.noreply.github.com>
2020-03-14 17:26:19 +0300

    Update Test.java

--- simple-sample/src/main/java/com/test/Test.java
+++ simple-sample/src/main/java/com/test/Test.java
@@ -5,9 +5,5 @@ class Test {
   public static void main(String[] args) {
     System.out.println("Hello, MNT Lab");
   }
-  
-  public static void main(String[] args) {
-    System.out.println("Hello, MNT Lab");
-  }
 
 }
