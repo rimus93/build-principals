7674929346b1a11e9cd24dbf67599221dae91b0c
rimus93 <45934179+rimus93@users.noreply.github.com>
2020-03-14 16:20:14 +0300

    Update Test.java

--- simple-sample/src/main/java/com/test/Test.java
+++ simple-sample/src/main/java/com/test/Test.java
@@ -4,7 +4,9 @@ class Test {
 
   public static void main(String[] args) {
     System.out.println("Hello, MNT Lab");
-    {some bad code 1+2-4/\?//???///??/}}}}
+    {some bad code 
+      Print void
+      static dynamic}
   }
 
 }
