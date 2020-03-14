c6ae661e7b8701008c610d3b6434c1c7e620e056
rimus93 <45934179+rimus93@users.noreply.github.com>
2020-03-14 16:30:04 +0300

    Update Test.java

--- simple-sample/src/main/java/com/test/Test.java
+++ simple-sample/src/main/java/com/test/Test.java
@@ -1,29 +1,7 @@
 package com.test;
 
 class Test {
-
-  public class ComplicatedDataHolder {
-    public final String data;
-    public final int num;
-
-    public static class Builder {
-        private String data;
-        private int num;
-        
-        public Builder data(String data) {
-            this.data = data;
-            return this;
-        }
-
-        public Builder num(int num) {
-            this.num = num;
-            return this;
-        }
-
-        public ComplicatedDataHolder build() {
-            return new ComplicatedDataHolder(data, num);
-        }  
+    public static main(String[] args) {
+        System.out.println("Welcome to Java);
     }
-}ЪЪ
-
 }
